617759c042728204682b7339024f402881827d80
Anish <96220428+Anishsuresh3@users.noreply.github.com>
2023-09-24 03:18:04 +0530

    Add files via upload

---
+++ RefineYourself.dart
@@ -0,0 +1,147 @@
+import 'dart:async';
+
+import 'package:another_stepper/dto/stepper_data.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_animation_progress_bar/flutter_animation_progress_bar.dart';
+
+class RefineYourself extends StatefulWidget {
+  const RefineYourself({Key? key}) : super(key: key);
+
+  @override
+  State<RefineYourself> createState() => _RefineYourselfState();
+}
+
+class _RefineYourselfState extends State<RefineYourself> {
+  int _currentValue = 0;
+  late Timer _timer;
+  bool _isRunning = false;
+  int _remainingTime = 120;
+  String formattedTime = "";
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  void _startProgress() {
+    if (!_isRunning) {
+      _isRunning = true;
+      _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+        setState(() {
+          if (_currentValue < 120) {
+            _currentValue++;
+            _remainingTime--;
+            int minutes = _remainingTime ~/ 60; // ~/ is used for integer division
+            int seconds = _remainingTime % 60;
+            formattedTime = '$minutes:${seconds.toString().padLeft(2, '0')}';// Decrement remaining time
+          } else {
+            _timer.cancel();
+            _isRunning = false;
+          }
+        });
+      });
+    }
+  }
+
+  void _pauseProgress() {
+    if (_isRunning) {
+      _timer.cancel();
+      _isRunning = false;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Color(0xFFF9CDCE),
+        appBar: AppBar(
+          leading: IconButton(
+            icon: ImageIcon(AssetImage("images/back_button.png")),
+            onPressed: (){},
+          ),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text("Easing Depression",style: TextStyle(
+                  fontSize: 30,color: Colors.black
+              ),),
+            ],
+          ),
+          backgroundColor: Color(0xFFF9CDCE),
+          elevation: 0,
+          toolbarHeight: 70,
+        ),
+        body: Container(
+          padding: EdgeInsets.only(left: 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 10,),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(width: 20,),
+                  Container(
+                    height: 500,
+                    child: Stack(
+                      children: [
+                        SizedBox(width: 10,),
+                        FAProgressBar(
+                          direction: Axis.vertical,
+                          verticalDirection: VerticalDirection.down,
+                          currentValue: _currentValue.toDouble(),
+                          maxValue: 120,
+                          size: 10,
+                          progressColor: Color(0xFFDEA5A7),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(width: 20,),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Step 1:\nMindfulness Meditation for Self-Compassion\n(10 minutes):",style: TextStyle(fontSize: 15,),),
+                      cont("Close your eyes and take a few deep breaths.Focus on your breath as you inhale and exhale."),
+                      cont("Shift your attention to your breath. Inhale deeply through your nose for a count of 4 seconds."),
+                      cont("Exhale slowly through your mouth for a count of 6 seconds."),
+                      SizedBox(height: 30,),
+                      Text("Step 2:\nGratitude Journaling (5 minutes):",style: TextStyle(fontSize: 15,),),
+                      cont("Write down three things you're grateful for today, no matter how small."),
+                      cont("Reflect on the positive aspects of your life and focus on moments of joy or beauty."),
+                      SizedBox(height: 30,),
+                      Text("Step 3:\nMeditation for Setting and Achieving Goals\n(20 minutes):",style: TextStyle(fontSize: 15,),),
+                      cont("Visualize your goals with clarity, imagining each step needed to achieve them."),
+                      cont("Cultivate determination and a sense of purpose."),
+                      cont("Cultivate determination and a sense of purpose."),
+                      SizedBox(height: 20,),
+                      _isRunning==true? Text("Remaining Time: ${formattedTime}",style: TextStyle(fontSize: 14,),):Text(""),
+                      _isRunning!=true?ElevatedButton(
+                        style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.purple)),
+                        onPressed: _startProgress,
+                        child: Text("Start Exercise"),
+                      ):Row(
+                        children: [
+                          IconButton(onPressed: _startProgress, icon: Icon(Icons.play_circle)),
+                          IconButton(onPressed: _pauseProgress, icon: Icon(Icons.pause_circle)),
+                        ],
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ],
+          ),
+        )
+    );
+  }
+  cont(String msg){
+    return Container(
+      constraints:  BoxConstraints (
+        maxWidth:  300,
+      ),
+      child: Text("•$msg",style: TextStyle(fontSize: 15,color: Colors.blueGrey)),
+    );
+  }
+}
